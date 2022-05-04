1f6bb22eddbd5db4912e4ca687c3575e873b7066
leonid-tkach <79228337+leonid-tkach@users.noreply.github.com>
2022-05-04 09:30:33 -0400

    print rows from csv

--- PyPoll.py
+++ PyPoll.py
@@ -3,13 +3,16 @@ import os
 
 # Assign a variable for the file to load and the path.
 file_to_load = os.path.join("Resources", "election_results.csv")
-# Open the election results and read the file
-with open(file_to_load) as election_data:
-  # To do: perform analysis.
-  print(election_data)
-
 # Create a filename variable to a direct or indirect path to the file.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+  # Read the file object with the reader function.
+  file_reader = csv.reader(election_data)
+  # Print each row in the CSV file.
+  headers = next(file_reader)
+  print(headers)
 # Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
   # Write three counties to the file.
