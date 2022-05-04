e8df908173dec800996ff22bac1b846711bd171d
leonid-tkach <79228337+leonid-tkach@users.noreply.github.com>
2022-05-03 21:12:26 -0400

    3.4.3 Write to Files with Python

--- PyPoll.py
+++ PyPoll.py
@@ -8,6 +8,14 @@ with open(file_to_load) as election_data:
   # To do: perform analysis.
   print(election_data)
 
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Using the with statement open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+  # Write three counties to the file.
+  txt_file.write("Counties in the Election\n")
+  txt_file.write("------------------------\n")
+  txt_file.write("Arapahoe\nDenver\nJefferson")
 
 # The data we need to retrieve.
 # 1. The total number of votes cast 
@@ -18,4 +26,3 @@ with open(file_to_load) as election_data:
 
 # Import the datetime class from the datetime module.
 # import datetime as dt
-
